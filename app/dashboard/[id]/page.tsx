3c219d9bc33c0648c89e97e856fb81745cc1730e
convenibitephp <convenibitephp@gmail.com>
2025-04-06 17:30:44 +0900

    Vercelテスト

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,11 +1,13 @@
 import CoinDetail from '@/app/ui/dashboard/coin-detail';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page({
   params,
 }: {
   params: { id: string };
 }) {
-  const id = (await params).id;
+  const { id } = await Promise.resolve(params);
 
   return (
     <main>
