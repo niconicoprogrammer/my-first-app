50d1088678853d2bc59708f2c60eb4646bbe062b
convenibitephp <convenibitephp@gmail.com>
2025-04-09 12:34:46 +0900

    Vercelテスト

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import LoginForm from '@/app/ui/login-form';
 
 export default function Page() {
   return (
     <main>
-      <LoginForm />
+      <Suspense fallback={<div>ログイン画面を読み込み中...</div>}>
+        <LoginForm />
+      </Suspense>
     </main>
   );
 }
