c7bd8196abb2e6b9b945f4ada765727a041900c2
convenibitephp <convenibitephp@gmail.com>
2025-04-12 23:17:31 +0900

    ログイン時制御

--- app/lib/supabase/middleware.ts
+++ app/lib/supabase/middleware.ts
@@ -49,6 +49,16 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
+  if (
+    user &&
+    !request.nextUrl.pathname.startsWith('/dashboard') 
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone()
+    url.pathname = '/dashboard'
+    return NextResponse.redirect(url)
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
