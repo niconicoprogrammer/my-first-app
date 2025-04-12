992e6bc069b154be120bfb086a3aee6d6b4c3965
convenibitephp <convenibitephp@gmail.com>
2025-04-12 23:11:10 +0900

    Vercelテスト

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import NavBar from '@/app/ui/dashboard/nav-bar';
 import BreadCrumb from '@/app/ui/dashboard/bread-crumb';
+import { Suspense } from "react";
 
 export const experimental_ppr = true;
 
@@ -7,7 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-[#0f172a] text-white">
       {/* ナビゲーションバー */}
-      <NavBar />
+      <Suspense>
+        <NavBar />
+      </Suspense>
 
       {/* コンテンツ全体を中央に揃える */}
       <div className="max-w-screen-xl mx-auto px-4 py-6 space-y-6">
